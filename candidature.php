4a05c7381d3a5c8e82f12cdfedc5f367a72e5b8f
Nasrallah SOULEYMANE <nasouleymane@outlook.com>
2026-04-08 15:02:27 +0100

    feat: structure HTML et initialisation des variables

---
+++ candidature.php
@@ -0,0 +1,27 @@
+
+<?php
+$prenom    = '';
+$nom       = '';
+$email     = '';
+$age       = '';
+$filiere   = '';
+$motivation = '';
+$reglement  = false;
+$erreurs   = [];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Candidature - Club Scolaire</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Formulaire de candidature</h1>
+        <p class="subtitle">Club Informatique de l'école</p>
+    </div>
+    
+</body>
+</html>
